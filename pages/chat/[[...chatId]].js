2448673def6875116559d761c3e4f5528b8285e3
Yemelyanov <nikolaef.post@gmail.com>
2023-08-22 20:20:39 +0300

    feat  auth styling

---
+++ pages/chat/[[...chatId]].js
@@ -0,0 +1,15 @@
+
+import Head from "next/head";
+
+const ChatId = () => {
+    return (
+        <div>
+            <Head>
+                <title>Next JS ChatGPT Starter</title>
+            </Head>
+            <h1>Welcome to chatId page</h1>
+        </div>
+    );
+};
+
+    export default ChatId;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,36 @@
 import Head from "next/head";
+import Link from "next/link";
+import {useUser} from "@auth0/nextjs-auth0/client";
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Next JS ChatGPT Starter</title>
-      </Head>
-      <h1>Welcome to the Next JS &amp; ChatGPT Starter R</h1>
-    </div>
-  );
+
+    const {isLoading, error, user} = useUser();
+
+    if (isLoading) return <div>Loading...</div>
+    if (isLoading) return <div>{error.message}</div>
+    return (
+        <>
+            <Head>
+                <title>Chatty Pete - Login or Signup</title>
+            </Head>
+            <div className="flex justify-center items-center min-h-screen w-full bg-gray-800 text-white text-center">
+                <div>
+                    {!!user && <Link href="/api/auth/logout">Logout</Link>}
+                    {!user && (
+                        <>
+                            <Link
+                                className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600 "
+                                href="/api/auth/login"
+                            >Login</Link>
+                            <Link
+                                className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600 "
+                                href="/api/auth/signup"
+                            >Signup</Link>
+                        </>
+                    )}
+                </div>
+
+            </div>
+        </>
+    );
 }
